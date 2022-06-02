5903560e211c090c7b695522b6beea3acf773fd5
Gabriel Fratelli <gfc@lionx.com.br>
2022-06-02 17:49:26 -0300

    chore: aplication react-hook-form

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-// import { useForm } from "react-hook-form";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import Icon from "../../assets/icon/google_icon.svg";
 
@@ -19,7 +19,36 @@ import {
   Footer,
 } from "./styles";
 
+type Inputs = {
+  email: string;
+  password: string;
+};
+
+const schema = yup
+  .object({
+    email: yup
+      .string()
+      .email("Digite um Email válido")
+      .required("Este campo é obrigatório"),
+    password: yup
+      .string()
+      .min(8, "A senha deve ter no mínimo 8 caracteres")
+      .required("Este campo é obrigatório"),
+  })
+  .required();
+
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit(userData: Inputs) {}
+
   return (
     <Container>
       <ImageContainer />
@@ -29,14 +58,24 @@ export const Login = () => {
             <span>Welcome back</span>
             <strong>Login to your account</strong>
           </Title>
-          <Form>
+          <Form onSubmit={handleSubmit(onSubmit)}>
             <label>
               <span>Email</span>
-              <input type="text" />
+              <input
+                type="text"
+                placeholder="Enter your Email"
+                {...register("email", { required: true })}
+              />
+              {errors.email && <p>{errors.email?.message}</p>}
             </label>
             <label>
               <span>Password</span>
-              <input type="password" />
+              <input
+                type="password"
+                placeholder="Inform your Password"
+                {...register("password", { required: true })}
+              />
+              {errors.password && <p>{errors.password?.message}</p>}
             </label>
           </Form>
           <ButtonRemember>
@@ -48,7 +87,7 @@ export const Login = () => {
             <button id="login" type="submit">
               Login Now
             </button>
-            <button id="google" type="submit">
+            <button id="google" type="button">
               <img src={Icon} alt="Icon Google" />
               Or sign with google
             </button>
